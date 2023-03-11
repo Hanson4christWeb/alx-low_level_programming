844ea57121cb014682243ff020c1d2fc059fa723
Eyuren Nyerovwo Hanson <peacestep4@gmail.com>
2023-03-10 16:03:27 -0800

    hot fix

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,25 +1,11 @@
-#include "main.h"
-
-void _puts(char *str)
-
-#include<stdio.h>
-
+#include <stdio.h>
 /**
-
- * main - Entry point
-
+ *  main -Entry point
  *
-
- * Return: Always 0 (Success)
-
+ *  Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-
         puts("\"Programming is like building a multilingual puzzle");
-
         return (0);
-
 }
